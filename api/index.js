ecded82d44521b9c1e9d90c0ad91ce50838274bc
Jyot <vasavajyotkumar@gmail.com>
2025-07-14 16:06:33 +0530

    fgo

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
     "http://localhost:5173",
-    "https://estate-networks.vercel.app/"
+    "https://estate-networks.netlify.app"
   );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
