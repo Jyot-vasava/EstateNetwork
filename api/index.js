e5c3b5490bcd36526fa4642ca2a0908a9f5e8586
Jyot <vasavajyotkumar@gmail.com>
2025-07-14 14:34:10 +0530

    changes for deploy

--- api/index.js
+++ api/index.js
@@ -22,7 +22,11 @@ app.use("/api/listings", listingRoutes);
 
 // CORS middleware
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:5173",
+    "https://estate-networks.vercel.app/"
+  );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
