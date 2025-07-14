5466d1dde2184a3627d710c201092b206e107419
Jyot <vasavajyotkumar@gmail.com>
2025-07-14 15:35:25 +0530

    kh

--- client/src/components/RAuth.jsx
+++ client/src/components/RAuth.jsx
@@ -16,7 +16,7 @@ const RAuth = () => {
 
       const result = await signInWithPopup(auth, provider);
 
-      const res = await fetch("/api/auth/google", {
+      const res = await fetch("${config.BACKEND_API}/api/auth/google", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
